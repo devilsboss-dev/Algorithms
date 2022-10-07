1d7e66c676a90ff8501c2f3e32287f3fdddcb048
Kartik Batra <85002344+devilsboss-dev@users.noreply.github.com>
2022-10-07 00:39:15 -0700

    Create c++fibonacci.cpp

---
+++ c++fibonacci.cpp
@@ -0,0 +1,25 @@
+// Fibonacci Series up to n number of terms
+// C++ program to Display Fibonacci Series
+#include<iostream>
+using namespace std;
+int main()
+{
+  // declare variables
+  int n, i, a=0, b=1, c;
+
+  // take input
+  cout << "Enter the number of terms: ";
+  cin >> n;
+
+  // display Fibonacci Series
+  cout << "Fibonacci Series is: " << endl;
+  for (i=a; i<=n; i++)
+  {
+    cout << a << "  ";
+    c=a+b;
+    a=b;
+    b=c;
+  }
+
+  return 0;
+}
